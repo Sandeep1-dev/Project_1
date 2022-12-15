d799eacf73bf37657b5337cc4566e86a59f213a7
Sandeep <Sanbook1.0.0.0@gmail.com>
2022-12-15 18:14:16 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(Project());
+}
+
+class Project extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text("Helllo My App"),
+    );
+  }
+}
